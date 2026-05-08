a86b2082c67d92d1d77406be09d419abe028d8bc
Abraham Cepeda <abrahamgolf@gmail.com>
2026-05-08 15:57:56 -0600

    removed validation

--- lib/validation.ts
+++
@@ -1,9 +0,0 @@
-import type { ClassItem } from "../types";
-
-export function isInPast(cls: ClassItem): boolean {
-  return new Date(cls.datetime).getTime() < Date.now();
-}
-
-export function isFull(cls: ClassItem): boolean {
-  return cls.bookedUsers >= cls.capacity;
-}
